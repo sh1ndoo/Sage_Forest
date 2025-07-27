6786d4f63f038c415f2686c56f3a05b741ee90eb
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 10:35:47 +0700

    Update cat_spots.js

--- marker_logic/cat_spots.js
+++ marker_logic/cat_spots.js
@@ -4,7 +4,7 @@ function addCatSpots(map) {
         create_checkbox: true,
         create_feature_popup: true,
         is_default: true,
-        // sidebar_icon_html: '<img class="sidebar-image" src="images/icons/marker_1.svg" />'
+        sidebar_icon_html: '<img class="sidebar-image" src="images/icons/marker_1.svg" />',
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
                 icon: Utils.getCustomIcon(undefined, "1"),
