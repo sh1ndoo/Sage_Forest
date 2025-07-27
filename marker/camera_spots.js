c8c3d35de63e16002de4a57c8c18a196f8bd9ede
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 13:18:41 +0700

    Create camera_spots.js

---
+++ marker/camera_spots.js
@@ -0,0 +1,75 @@
+var camera_spots = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "01",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    216.75,
+                    -72
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "02",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    160,
+                    -143.75
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "03",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    131.25,
+                    -174.375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "04",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    113.25,
+                    -233.75
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "05",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    57.53125,
+                    -170.125
+                ]
+            }
+        }
+    ]
+}
