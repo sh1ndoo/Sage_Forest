3db8a4a255396452fd0d94cb73cb575dc62d39de
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 13:19:40 +0700

    Create bubble_cat.js

---
+++ marker/bubble_cat.js
@@ -0,0 +1,47 @@
+var bubble_cat = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "01",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    215.5,
+                    -77.75
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "02",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    150.25,
+                    -86.25
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "03",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    47.25,
+                    -189
+                ]
+            }
+        }
+    ]
+}
